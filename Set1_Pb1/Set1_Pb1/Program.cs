bcb364acd35a6db6c1a49b5c92dec0512c1334f8
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 15:07:56 +0200

    Set1_Pb1

---
+++ Set1_Pb1/Set1_Pb1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set1_Pb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti doua numere");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            double x;
+            if (a == 0)
+                Console.WriteLine("Ecuatia nu are solutie");
+            else
+            {
+                if (b == 0)
+                    Console.WriteLine("Solutia ecuatiei este: x=0");
+                else
+                {
+                    x = -b / a;
+                    Console.WriteLine($"Solutia ecuatiei este: x= {x}");
+                }
+            }
+        }
+    }
+}
