f28b7d109ac2be331c33b61cbb1a0cd867f47ca6
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 17:13:57 +0200

    swap aritmetic

---
+++ Set1_Pb8/Set1_Pb8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Set1_Pb8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti doua numere:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"Dupa interschimbare: {a} , {b} ");
+        }
+    }
+}
