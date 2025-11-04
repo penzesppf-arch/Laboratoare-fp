c7df9a8f9909e1861489cdf814a84cb6263cbb8e
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 17:04:40 +0200

    swap prin var

---
+++ Set1_Pb7/Set1_Pb7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set1_Pb7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti doua numere");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int swap=a;
+            a = b;  
+            b = swap;
+            Console.WriteLine($"Dupa interschimbare: {a} , {b}");
+
+        }
+    }
+}
