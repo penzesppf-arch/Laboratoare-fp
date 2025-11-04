3e3b71b9c9f49e1f9ad82f169cc31684c1fc6ec0
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 16:02:44 +0200

    extragere cifra de pe poz k

---
+++ Set1_Pb5/Set1_Pb5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set1_Pb5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti un numae k:");
+            int k = int.Parse(Console.ReadLine());
+            int copieN = n;
+            int lungime = 0;
+            while (copieN != 0)
+            {
+                lungime++;
+                copieN /= 10;
+            }
+            if (k > lungime)
+            {
+                Console.WriteLine("k nu poate fi mai mare decat numarul de cifre al numarului n");
+            }
+            else
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    n /= 10;
+                }
+                int cifra = n % 10;
+                Console.WriteLine($"Rezultatul este:{cifra}");
+
+            }
+        }
+    }
+}
