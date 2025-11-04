4a63ef56dd80289cd82e9a524b97cd51ab1d2218
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 16:59:28 +0200

    poate fi triunghi

---
+++ Set1_Pb6/Set1_Pb6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Set1_Pb6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti 3 numere POZITIVE:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a == 0 || b == 0 || c == 0)
+                Console.WriteLine("Nu poate fi triunghi.");
+            else
+                if (a + b > c || a + c > b || b + c > a)
+                Console.WriteLine("Poate fi triunghi.");
+            else
+                Console.WriteLine("Nu poate fi triunghi.");
+        }
+    }
+}
