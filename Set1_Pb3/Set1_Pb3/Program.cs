424ec92f114bcdc5ebb2005c4d0a0f64494dde59
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 15:37:16 +0200

    divizibilitate

---
+++ Set1_Pb3/Set1_Pb3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Set1_Pb3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti doua numere");
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            if (k == 0)
+                Console.WriteLine("Zero nu poate fi divizor");
+            else
+                if (n % k == 0)
+            {
+                Console.WriteLine($"{n} se divide cu {k}");
+            }
+                else
+                {
+                Console.WriteLine($"{n} nu se divide cu {k}");
+                }
+
+        }
+    }
+}
