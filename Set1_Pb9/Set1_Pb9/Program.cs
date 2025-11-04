dcbec0d05ce430d767d87e07c16ef1b02897201b
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 17:59:38 +0200

    divizori

---
+++ Set1_Pb9/Set1_Pb9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Set1_Pb9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar pozitiv ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> divizori = new List<int>();
+            for (int d = 1; d * d <= n; d++)
+            {
+                if (n % d == 0)
+                {
+                    divizori.Add(d);
+                    if (d != n / d)
+                        divizori.Add(n / d);
+                }
+            }
+            divizori.Sort();
+            Console.WriteLine($"Divizorii lui {n} sunt :");
+            Console.WriteLine(string.Join(" ", divizori));
+        }
+        
+    }
+}
