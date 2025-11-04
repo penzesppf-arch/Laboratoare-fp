607d2b87ff5a07b131ba655b3f36cfca058b56ab
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-04 15:48:12 +0200

    an bisect

---
+++ Set1_Pb4/Set1_Pb4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set1_Pb4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti anul");
+            int an = int.Parse(Console.ReadLine());
+            if ((an % 4 == 0 && an % 100 != 0) || (an % 400 == 0))
+            {
+                Console.WriteLine("Anul {0} este an bisect.", an);
+            }
+            else
+            {
+                Console.WriteLine("Anul {0} nu este an bisect.", an);
+            }
+        }
+    }
+}
