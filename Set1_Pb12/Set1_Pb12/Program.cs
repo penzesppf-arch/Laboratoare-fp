81942168de0fd2f18998ac7c2861cc572dd1bfde
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-05 18:06:23 +0200

    ?nr div cu n pe [a,b]

---
+++ Set1_Pb12/Set1_Pb12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Set1_Pb12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti trei numere");
+            int n = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int contor = 0;
+            int swap;
+            if (a > b)
+            {
+                swap = a;
+                a = b;
+                b = swap;
+            }
+            for (int i = a; i <= b; i++)
+            {
+                if (i % n == 0)
+                {
+                    contor++;
+                }
+
+            }
+            Console.WriteLine($"Numarul de divizori este: {contor}");
+
+        }
+    }
+}
