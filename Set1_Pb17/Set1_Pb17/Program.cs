6836d699a83b3f841f0a5344b374b01abd1b72bb
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-05 21:08:21 +0200

    cmmdc & cmmmc

---
+++ Set1_Pb17/Set1_Pb17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Set1_Pb17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti doua numere:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int rest = a % b;
+            int x = a;
+            int y = b;
+            while (rest != 0)
+            {
+                a = b;
+                b = rest;
+                rest = a % b;
+            }
+            int cmmmc = (x * y) / b;
+            Console.WriteLine($"Cmmdc este {b} si cmmmc este {cmmmc}");
+        }
+    }
+}
