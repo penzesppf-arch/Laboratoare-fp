93e57298ea48880df2ae6dc82d6968c4a72443ae
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-05 18:15:51 +0200

    nr ani bisecti intre an1 si an2

---
+++ Set1_Pb13/Set1_Pb13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set1_Pb13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti doi ani");
+            int an1 = int.Parse(Console.ReadLine());
+            int an2 = int.Parse(Console.ReadLine());
+            int contor = 0;
+            for (int an = an1; an <= an2; an++)
+            {
+                if ((an % 4 == 0 && an % 100 != 0) || (an % 400 == 0))
+                {
+                    contor++;
+                }
+               
+            } 
+            Console.WriteLine($"Numarul de ani bisecti intre {an1} si {an2} este: {contor}");
+        }
+    }
+}
