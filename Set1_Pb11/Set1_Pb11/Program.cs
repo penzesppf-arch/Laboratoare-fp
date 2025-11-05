4767af14e8132867d31d3e8dbac6446fb767bdd9
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-05 17:46:19 +0200

    afis cifre ord inv

---
+++ Set1_Pb11/Set1_Pb11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Set1_Pb11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar");
+            int n = int.Parse(Console.ReadLine());
+            int cifra;
+            if (n == 0)
+                Console.WriteLine("0");
+            else
+                while (n != 0)
+                {
+                    cifra = n % 10;
+                    Console.WriteLine(cifra);
+                    n = n / 10;
+                }
+        }
+    }
+}
