26b5f0dc0effe81c3ff62b34a6d77ae339d476d1
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-17 15:02:01 +0200

    desc in factori primi

---
+++ Set1_Pb18/Set1_Pb18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Set1_Pb18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+            int putere = 0;
+            int divizor = 2;
+            while (n > 1)
+            {
+                if (n % divizor == 0)
+                {
+                    putere++;
+                    n /= divizor;
+                }
+                else
+                {
+                    if (putere > 0)
+                    {
+                        Console.Write($"{divizor} ^ {putere} * ");
+                    }
+                    divizor++;
+                    putere = 0;
+                }
+            }
+            if (putere > 0)
+            {
+                Console.Write($"{divizor} ^ {putere}");
+            }
+            
+        }
+    }
+}
