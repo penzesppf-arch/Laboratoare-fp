34b59ce03259718e5452c69e8ca678ddf222930e
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-17 16:07:08 +0200

    fractie

---
+++ Set1/Set1_Pb20/Set1_Pb20/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set1_Pb20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti m si n: ");
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int parteInt, parteFract;
+            parteInt = m / n; 
+            parteFract = m % n;
+            Console.Write($"{parteInt},");
+            int cifra, rest;
+            List<int> resturi = new List<int>();
+            List<int> cifre = new List<int>();
+            resturi.Add(parteFract);
+            bool periodic = false;
+            do
+            {
+                cifra = parteFract * 10 / n;
+                cifre.Add(cifra);
+                rest = parteFract * 10 % n;
+                if (!resturi.Contains(rest))
+                {
+                    resturi.Add(rest);
+                }
+                else
+                {
+                    periodic = true;
+                    break;
+                }
+                parteFract = rest;
+            } while (rest != 0);
+
+            if (!periodic)
+            {
+                foreach (var item in cifre)
+                {
+                    Console.Write(item);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < resturi.Count; i++)
+                {
+                    if (resturi[i] == rest)
+                    {
+                        Console.Write("(");
+                    }
+                    Console.Write(cifre[i]);
+                }
+                Console.WriteLine(")");
+            }
+        }
+    }
+    }
+
