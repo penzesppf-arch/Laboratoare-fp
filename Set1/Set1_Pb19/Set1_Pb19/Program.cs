80e4346d5ea9109b3d113b5c584f1a565eded2a7
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-17 15:39:35 +0200

    nr doar din 2 cifre

---
+++ Set1/Set1_Pb19/Set1_Pb19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Set1_Pb19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar:");
+            string n = Console.ReadLine();
+            HashSet<char> cifre = new HashSet<char>();
+            foreach(char c in n)
+            {
+                if (char.IsDigit(c))
+                    cifre.Add(c);
+            }
+            if (cifre.Count == 2)
+                Console.WriteLine("Numarul este format doar din 2 cifre");
+            else
+                Console.WriteLine("Numarul NU este format doar din 2 cifre");
+
+        }
+    }
+}
