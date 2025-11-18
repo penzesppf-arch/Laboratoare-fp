47c3654f93969439b021feba08cf0d1f3a474ba5
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 21:00:53 +0200

    fib

---
+++ Set2/Set2_Pb8/Set2_Pb8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Set2_Pb8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intruduceti un numar");
+            int n = int.Parse(Console.ReadLine());
+            long a = 0, b = 1, c;
+            for (int i = 2; i <= n; i++)
+            {
+                
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
