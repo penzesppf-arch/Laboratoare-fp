7b7e070c9f335b26c6931c4642488d6ba4871e21
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 21:41:42 +0200

    monoton

---
+++ Set2/Set2_Pb9/Set2_Pb9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Set2_Pb9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere");
+            string sir = Console.ReadLine();
+            string[] numere = sir.Split(' ');
+            int[] v=new int[numere.Length];
+            for (int i = 0; i < numere.Length; i++)
+            {
+                v[i] = int.Parse(numere[i]);
+            }
+            bool esteCrescator = true;
+            bool esteDescrescator = true;
+            for (int i = 0; i < numere.Length -1; i++)
+            {
+                if (v[i] < v[i + 1])
+                {
+                    esteDescrescator = false;
+                }
+                if(v[i] > v[i + 1])
+                {
+                    esteCrescator = false;
+                }
+
+            }
+            if (esteCrescator || esteDescrescator)
+            {
+                Console.WriteLine("Secventa este monotona");
+            }
+            else
+            { 
+                Console.WriteLine("Secventa nu este monotona");
+            }
+        }
+    }
+}
+
---
+++ Set2/Set2_Pb9/Set2_Pb9/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Set2_Pb9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Set2_Pb9")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8bafcdc-63ae-4971-92c1-6e4a756598d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
