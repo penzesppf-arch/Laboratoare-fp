897b001b6cb1406c78edaaa764812a597a1f0ff7
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 15:34:42 +0200

    ?poz ?neg ?null

---
+++ Set2/Set2_Pb2/Set2_Pb2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set2_Pb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti o secventa de n numere separate prin spatiu:");
+            string numere = Console.ReadLine();
+            string[] numereArray = numere.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+            int nrPozitive = 0;
+            int nrNegative = 0;
+            int nrNull = 0;
+            foreach (string nrString in numereArray)
+            {
+                if (int.TryParse(nrString, out int nr))
+                {
+                    if (nr > 0)
+                    {
+                        nrPozitive++;
+                    }
+                    else if (nr < 0)
+                    {
+                        nrNegative++;
+                    }
+                    else
+                    {
+                        nrNull++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"'{nrString}' nu este un numar intreg.");
+                }
+            }
+            Console.WriteLine($"Numere pozitive: {nrPozitive}, numere negative: {nrNegative} si care sunt zero :{nrNull}.");
+
+        }
+    }
+}
