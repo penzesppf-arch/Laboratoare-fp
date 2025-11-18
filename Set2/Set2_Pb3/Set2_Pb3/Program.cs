11423f3f7deca6aa84aa5f509179356a71aa110e
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 15:45:43 +0200

    suma & produs

---
+++ Set2/Set2_Pb3/Set2_Pb3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set2_Pb3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intrudoceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            int produs = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                suma += i;
+                produs *= i;
+            }
+            Console.WriteLine($"Suma este: {suma} si produsul este : {produs}");
+        }
+    }
+}
