a2e3c5f4f92781c9f86db871e1b824a254c1519a
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 17:21:41 +0200

    poz=nr

---
+++ Set2/Set2_Pb5/Set2_Pb5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Set2_Pb5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere separate prin spatii:");
+            string sir = Console.ReadLine();
+            string[] numere = sir.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            for (int i = 0; i < numere.Length; i++)
+            {
+                int num;
+                if (int.TryParse(numere[i], out num))
+                {
+                    if (num == i)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Atatea nr coincid cu pozitia lor : {count}.");
+        }
+    }
+}
