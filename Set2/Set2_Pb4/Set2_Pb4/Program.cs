9696821233ca9c651f915b15750165fe14e46afe
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 16:45:09 +0200

    ?poz a

---
+++ Set2/Set2_Pb4/Set2_Pb4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Set2_Pb4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti un sir de numere si un numar a");
+            string sir = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            string[] numere = sir.Split(' ');
+            int poz = Array.IndexOf(numere, a.ToString());
+            if (poz != -1)
+            {
+                Console.WriteLine($"Numarul {a} se afla pe pozitia {poz}");
+            }
+            else 
+                Console.WriteLine($" -1 ");
+        }
+
+    }
+}
