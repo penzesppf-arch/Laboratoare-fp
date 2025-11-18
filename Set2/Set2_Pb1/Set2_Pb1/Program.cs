1dfd68c8e2c91419e8f92b00ebbbb76e4a943f67
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-18 14:50:01 +0200

    ?pare

---
+++ Set2/Set2_Pb1/Set2_Pb1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set2_Pb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar n si o secventa de n numere:");
+            int n = int.Parse(Console.ReadLine());
+            int contor = 0;
+            for (int i = 0; i < n; i++)
+            {
+              int nr = int.Parse(Console.ReadLine());
+                if(nr % 2 == 0)
+                    contor++;
+            } 
+            Console.WriteLine($"Numarul de numere pare este: {contor}");
+        }   
+    }
+}
