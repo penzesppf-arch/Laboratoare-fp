bb38b129eae98f09f8bb600c80a15ae076e23c3d
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-20 16:52:02 +0200

    sum inv

---
+++ Set2/Set2_Pb11/Set2_Pb11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Set2_Pb11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un si de numere:");
+            string sir = Console.ReadLine();
+            string[] numere = sir.Split(' ');
+            int suma = 0;
+            foreach (string numar in numere)
+            {
+                suma += invers(int.Parse(numar));
+            }
+            Console.WriteLine($"Suma numerelor inversate este: {suma}");
+        }
+
+        static int invers(int n)
+        {
+            int inversat = 0;
+            while (n != 0)
+            {
+                int cifra = n % 10;
+                inversat = inversat * 10 + cifra;
+                n /= 10;
+            }
+            return inversat;
+        }
+    }
+}
