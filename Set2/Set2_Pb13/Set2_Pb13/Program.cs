a253f34df53e4cb35856703c3d87408ef935b170
Penzes Patricia <penzes.ppf@gmail.com>
2025-11-20 18:49:25 +0200

    secv cresc rotabila

---
+++ Set2/Set2_Pb13/Set2_Pb13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set2_Pb13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere");
+            string sir = Console.ReadLine();
+            string[] numere = sir.Split(' ');
+            int count = 0;
+            if (numere.Length <= 1)
+            {
+                Console.WriteLine("Sirul este o secventa crescatoare rotita.");
+                return;
+            }
+            int[] sirNumere= new int[numere.Length];
+            for (int i = 0; i < numere.Length; i++)
+            {
+                sirNumere[i] = int.Parse(numere[i]);
+            }
+            for (int i = 0; i < sirNumere.Length - 1; i++)
+            { 
+                if (sirNumere[i] > sirNumere[i + 1])
+                    count++;
+            }
+            if(sirNumere[sirNumere.Length - 1] > sirNumere[0])
+                count++;
+
+            if (count <= 1)
+                Console.WriteLine("Sirul este o secventa crescatoare rotita.");
+            else
+                Console.WriteLine("Sirul NU este o secventa crescatoare rotita.");
+        }
+    }
+}
