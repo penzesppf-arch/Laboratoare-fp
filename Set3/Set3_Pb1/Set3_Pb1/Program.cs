3b58a569c777d6c1d801bf440a894eb1c833d668
Penzes Patricia <penzes.ppf@gmail.com>
2025-12-08 14:21:19 +0200

    sum vectori

---
+++ Set3/Set3_Pb1/Set3_Pb1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set3_Pb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numere separate prin spatii.");
+            string[] v;
+            v = Console.ReadLine().Split(' ');
+            int s = 0;
+            for (int i = 0; i < v.Length; i++)
+            {
+                s = s + int.Parse(v[i]);
+            }
+            Console.WriteLine($"Suma numerelor este: {s}");
+
+        }
+    }
+}
