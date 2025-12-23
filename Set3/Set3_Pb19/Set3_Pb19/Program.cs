2648d5a17d9fa07c771eaeca89fbc49f64eef2d2
Penzes Patricia <penzes.ppf@gmail.com>
2025-12-23 19:32:12 +0200

    nr aparitii

---
+++ Set3/Set3_Pb19/Set3_Pb19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Set3_Pb19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere s separate prin spatii:");
+            string input = Console.ReadLine();
+            int[] s = Array.ConvertAll(input.Split(' '), int.Parse);
+            Console.WriteLine("Introduceti un sir de numere p separate prin spatii:");
+            string cauta = Console.ReadLine();
+            int[] p = Array.ConvertAll(cauta.Split(' '), int.Parse);
+
+            int numarAparitii = 0;
+            for (int i = 0; i <= s.Length - p.Length; i++)
+            {
+                bool estePotrivire = true;
+
+                for (int j = 0; j < p.Length; j++)
+                {
+                    if (s[i + j] != p[j])
+                    {
+                        estePotrivire = false;
+                        break;
+                    }
+                }
+
+                if (estePotrivire)
+                {
+                    numarAparitii++;
+                }
+            }
+
+            Console.WriteLine($"Vectorul p apare in s de {numarAparitii} ori.");
+        }
+    }
+}
