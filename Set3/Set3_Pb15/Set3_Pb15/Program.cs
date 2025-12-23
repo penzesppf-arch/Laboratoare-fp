adffaaf518710370c21242d40e3f585f104e5173
Penzes Patricia <penzes.ppf@gmail.com>
2025-12-23 16:36:09 +0200

    fara repetare in vector

---
+++ Set3/Set3_Pb15/Set3_Pb15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Set3_Pb15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere separate prin spatii:");
+            string sir = Console.ReadLine();
+            int[] numere = Array.ConvertAll(sir.Split(' '), int.Parse);
+            Array.Sort(numere);
+            int k = 0;
+            for (int i = 1; i < numere.Length; i++)
+            {
+                if (numere[i] != numere[k])
+                {
+                    k++;
+                    numere[k] = numere[i];
+                }
+            }
+            int dimensiune = k + 1;
+            Console.WriteLine("Sirul fara duplicate este:");
+            for (int i = 0; i < dimensiune; i++)
+            { Console.Write(numere[i] + " "); }
+        }
+    }
+}
