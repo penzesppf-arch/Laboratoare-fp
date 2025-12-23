04b52f7d89a7e30eb0d57c055c9cd4be994d095c
Penzes Patricia <penzes.ppf@gmail.com>
2025-12-23 16:03:22 +0200

    selection sort

---
+++ Set3/Set3_Pb12/Set3_Pb12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set3_Pb12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere separate prin spatiu:");
+            string input = Console.ReadLine();
+            int[] arr = Array.ConvertAll(input.Split(' '), int.Parse);
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+            Console.WriteLine("Sirul sortat este:");
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
