d129596d7c0cf8cf77cd7d60a7e3eec3820107f7
Penzes Patricia <penzes.ppf@gmail.com>
2025-12-23 15:24:26 +0200

    cautare binara

---
+++ Set3/Set3_Pb10/Set3_Pb10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Set3_Pb10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un sir de numere CRESCATOARE:");
+            string input = Console.ReadLine();
+            int[] numbers = Array.ConvertAll(input.Split(' '), int.Parse);
+            Console.WriteLine("Introduceti un numar:");
+            int k = int.Parse(Console.ReadLine());
+            int pozitie = BinarySearchManual(numbers, k);
+            Console.WriteLine($"Pozitia numarului {k} este {pozitie}");
+        }
+        static int BinarySearchManual(int[] arr, int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1; 
+        }
+    }
+}
