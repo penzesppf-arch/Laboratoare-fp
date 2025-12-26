20ab5e4ae7aa68503f85a43a707f2ef7a0cab3f0
Penzes Patricia <penzes.ppf@gmail.com>
2025-12-26 16:39:54 +0200

    element majoritate

---
+++ Set3/Set3_Pb31/Set3_Pb31/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set3_Pb31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceți elementele vectorului separate prin spațiu:");
+            int[] v = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray() ?? new int[0];
+
+            if (v.Length == 0) return;
+            int candidat = 0;
+            int contor = 0;
+            foreach (int x in v)
+            {
+                if (contor == 0)
+                {
+                    candidat = x;
+                    contor = 1;
+                }
+                else if (x == candidat)
+                {
+                    contor++;
+                }
+                else
+                {
+                    contor--;
+                }
+            }
+
+            int aparitii = 0;
+            foreach (int x in v)
+            {
+                if (x == candidat) aparitii++;
+            }
+
+            if (aparitii > v.Length / 2)
+            {
+                Console.WriteLine($"Elementul majoritate este: {candidat}");
+            }
+            else
+            {
+                Console.WriteLine("<nu exista>");
+            }
+        }
+    }
+    }
